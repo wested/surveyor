319539e1901ece5ec72b4af4c82861c2d65481f6
Diana Quintal <dquinta@wested.org>
2019-08-30 12:03:51 -0700

    fix to mustache helper...tests now passing

--- lib/surveyor/mustache_context.rb
+++ lib/surveyor/mustache_context.rb
@@ -1,12 +1,16 @@
 module Surveyor
   module MustacheContext
     extend ActiveSupport::Concern
-    
+
     def in_context(text, context=nil)
-      case context
-      when NilClass then text
-      when Hash     then Mustache.render(text, context)
-      else               context.render(text)
+      return unless text.present?
+
+      if context.is_a?(NilClass)
+        text
+      elsif context.is_a?(Hash)
+        Mustache.render(text, context)
+      else
+        context.render(text)
       end
     end
   end
